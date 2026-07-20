bf476aa5dbca0346aaa0fd83c083f243fa327629
edwardsaja <edwardbene07@gmail.com>
2026-07-21 00:12:54 +0800

    chore: sederhanain script animasi hp kamar biar gak pusing liatnya

--- Assets/Script/AnimasiHandphoneKamar.cs
+++ Assets/Script/AnimasiHandphoneKamar.cs
@@ -1,23 +1,18 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class AnimasiHandphoneKamar : MonoBehaviour
 {
     [Header("Waktu")]
-    public float waktuTunggu = 4.5f;
+    public float waktuTunggu = 4.0f; // di set 4 detik sesuai request
 
-    [Header("Visual")]
+    [Header("Visual Notifikasi")]
     public GameObject visualHpMenyala;
     
-    [Header("Zoom & Event")]
-    public bool gunakanZoomOtomatisScriptIni = true;
-    
-    public Camera kameraYangAkanDizoom;
-    public RectTransform panelMejaYangAkanDizoom;
+    [Header("Zoom Setup")]
+    public RectTransform panelMeja;
     public RectTransform titikFokusZoom;
-
     public float durasiZoom = 1.5f;
     public float targetSkalaZoom = 3f;
 
@@ -27,118 +22,78 @@ public class AnimasiHandphoneKamar : MonoBehaviour
     void Start()
     {
         // matiin layar pas mulai
-        if (visualHpMenyala != null)
-            visualHpMenyala.SetActive(false);
-
-        StartCoroutine(ProsesHandphoneMenyala());
+        if (visualHpMenyala != null) visualHpMenyala.SetActive(false);
+        StartCoroutine(ProsesMulai());
     }
 
-    IEnumerator ProsesHandphoneMenyala()
+    IEnumerator ProsesMulai()
     {
-        // 1. Tunggu 4 detik (atau sesuai waktuTunggu)
+        // 1. Tunggu beberapa detik
         yield return new WaitForSeconds(waktuTunggu);
 
-        // 2. Langsung jalankan Zoom duluan dan TUNGGU sampai selesai
-        if (gunakanZoomOtomatisScriptIni)
-        {
-            if (kameraYangAkanDizoom != null)
-                yield return StartCoroutine(AnimasiZoomKamera());
-            else if (panelMejaYangAkanDizoom != null)
-                yield return StartCoroutine(AnimasiZoomPanelMeja());
-        }
+        // 2. Jalankan zoom meja dan tunggu sampai beres
+        if (panelMeja != null)
+            yield return StartCoroutine(AnimasiZoomPanel());
 
-        // 3. Tunggu 1 detik setelah zoom SELESAI
+        // 3. Jeda 1 detik
         yield return new WaitForSeconds(1f);
 
-        // 4. Baru nyalain/munculin HP
-        if (visualHpMenyala != null)
-            visualHpMenyala.SetActive(true);
-
-        // 5. Jalankan event tambahan kalau ada
-        if (eventSaatHpMenyala != null)
-            eventSaatHpMenyala.Invoke();
+        // 4. Nyalain notifikasi
+        if (visualHpMenyala != null) visualHpMenyala.SetActive(true);
+        if (eventSaatHpMenyala != null) eventSaatHpMenyala.Invoke();
     }
 
-    IEnumerator AnimasiZoomPanelMeja()
+    IEnumerator AnimasiZoomPanel()
     {
-        RectTransform targetRect = titikFokusZoom != null ? titikFokusZoom : GetComponent<RectTransform>();
+        RectTransform target = titikFokusZoom != null ? titikFokusZoom : GetComponent<RectTransform>();
         Canvas canvas = GetComponentInParent<Canvas>();
         
-        Vector3 posisiTengahLayar = canvas != null ? 
+        Vector3 layarTengah = canvas != null ? 
             canvas.transform.TransformPoint(canvas.GetComponent<RectTransform>().rect.center) : 
             new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
 
-        Vector3 skalaAwal = panelMejaYangAkanDizoom.localScale;
-        Vector3 posisiAwal = panelMejaYangAkanDizoom.position;
+        Vector3 skalaAwal = panelMeja.localScale;
+        Vector3 posisiAwal = panelMeja.position;
         Vector3 skalaAkhir = skalaAwal * targetSkalaZoom;
+        float rasio = targetSkalaZoom / skalaAwal.x;
+        
+        // hitung posisi biar target ada di tengah layar
+        Vector3 offset = target.position - posisiAwal;
+        Vector3 posisiAkhir = layarTengah - (offset * rasio);
 
-        // cari target posisi
-        Vector3 offsetTargetAwal = targetRect.position - posisiAwal;
-        float rasioSkala = targetSkalaZoom / skalaAwal.x;
-        Vector3 posisiAkhir = posisiTengahLayar - (offsetTargetAwal * rasioSkala);
+        // cegah gambar panel jebol/keluar layar hitam (clamping)
+        Vector3[] batas = new Vector3[4];
+        canvas.GetComponent<RectTransform>().GetWorldCorners(batas);
+        float lebarLayar = batas[2].x - batas[0].x;
+        float tinggiLayar = batas[2].y - batas[0].y;
 
-        // cegah panel keluar dari batas layar
-        Vector3[] canvasCorners = new Vector3[4];
-        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-        canvasRect.GetWorldCorners(canvasCorners);
-        float screenW = canvasCorners[2].x - canvasCorners[0].x;
-        float screenH = canvasCorners[2].y - canvasCorners[0].y;
+        panelMeja.GetWorldCorners(batas);
+        float lebarPanel = (batas[2].x - batas[0].x) * rasio;
+        float tinggiPanel = (batas[2].y - batas[0].y) * rasio;
 
-        Vector3[] panelCorners = new Vector3[4];
-        panelMejaYangAkanDizoom.GetWorldCorners(panelCorners);
-        float panelW = (panelCorners[2].x - panelCorners[0].x) * rasioSkala;
-        float panelH = (panelCorners[2].y - panelCorners[0].y) * rasioSkala;
+        float batasX = Mathf.Max(0, (lebarPanel - lebarLayar) / 2f);
+        float batasY = Mathf.Max(0, (tinggiPanel - tinggiLayar) / 2f);
 
-        float marginX = Mathf.Max(0, (panelW - screenW) / 2f);
-        float marginY = Mathf.Max(0, (panelH - screenH) / 2f);
+        Vector3 pusatPanel = (batas[2] + batas[0]) / 2f;
+        Vector3 offsetPusat = (pusatPanel - posisiAwal) * rasio;
+        Vector3 posisiNetral = layarTengah - offsetPusat;
 
-        Vector3 currentPanelCenter = (panelCorners[2] + panelCorners[0]) / 2f;
-        Vector3 pivotToCenterAwal = currentPanelCenter - posisiAwal;
-        Vector3 pivotToCenterAkhir = pivotToCenterAwal * rasioSkala;
-        Vector3 neutralPos = posisiTengahLayar - pivotToCenterAkhir;
-
-        posisiAkhir.x = Mathf.Clamp(posisiAkhir.x, neutralPos.x - marginX, neutralPos.x + marginX);
-        posisiAkhir.y = Mathf.Clamp(posisiAkhir.y, neutralPos.y - marginY, neutralPos.y + marginY);
+        posisiAkhir.x = Mathf.Clamp(posisiAkhir.x, posisiNetral.x - batasX, posisiNetral.x + batasX);
+        posisiAkhir.y = Mathf.Clamp(posisiAkhir.y, posisiNetral.y - batasY, posisiNetral.y + batasY);
 
         // animasi jalan
-        float elapsed = 0f;
-        while (elapsed < durasiZoom)
+        float waktu = 0f;
+        while (waktu < durasiZoom)
         {
-            elapsed += Time.deltaTime;
-            float t = Mathf.SmoothStep(0f, 1f, elapsed / durasiZoom);
+            waktu += Time.deltaTime;
+            float t = Mathf.SmoothStep(0f, 1f, waktu / durasiZoom);
 
-            panelMejaYangAkanDizoom.localScale = Vector3.Lerp(skalaAwal, skalaAkhir, t);
-            panelMejaYangAkanDizoom.position = Vector3.Lerp(posisiAwal, posisiAkhir, t);
-
+            panelMeja.localScale = Vector3.Lerp(skalaAwal, skalaAkhir, t);
+            panelMeja.position = Vector3.Lerp(posisiAwal, posisiAkhir, t);
             yield return null;
         }
         
-        panelMejaYangAkanDizoom.localScale = skalaAkhir;
-        panelMejaYangAkanDizoom.position = posisiAkhir;
-    }
-
-    IEnumerator AnimasiZoomKamera()
-    {
-        float sizeAwal = kameraYangAkanDizoom.orthographicSize;
-        float sizeAkhir = sizeAwal / targetSkalaZoom;
-
-        Vector3 posisiAwalKamera = kameraYangAkanDizoom.transform.position;
-        Vector3 posisiHP = transform.position;
-        Vector3 posisiAkhirKamera = new Vector3(posisiHP.x, posisiHP.y, posisiAwalKamera.z);
-
-        float elapsed = 0f;
-        while (elapsed < durasiZoom)
-        {
-            elapsed += Time.deltaTime;
-            float t = Mathf.SmoothStep(0f, 1f, elapsed / durasiZoom);
-
-            kameraYangAkanDizoom.orthographicSize = Mathf.Lerp(sizeAwal, sizeAkhir, t);
-            kameraYangAkanDizoom.transform.position = Vector3.Lerp(posisiAwalKamera, posisiAkhirKamera, t);
-            
-            yield return null;
-        }
-
-        kameraYangAkanDizoom.orthographicSize = sizeAkhir;
-        kameraYangAkanDizoom.transform.position = posisiAkhirKamera;
+        panelMeja.localScale = skalaAkhir;
+        panelMeja.position = posisiAkhir;
     }
 }
